0e4d7eccdcd09c42920ad95963d7684c880ee306
lowjunen <lowjunen@gmail.com>
2020-11-01 21:29:00 -0800

    Working version of custom uORB node.

--- src/msl_gcs/src/traj_node.cpp
+++ src/msl_gcs/src/traj_node.cpp
@@ -8,21 +8,21 @@ int main(int argc, char **argv)
 
     ros::Publisher traj_pub = nh.advertise<mavros_msgs::TrajectoryNominal>("mavros/trajectory_nominal/trajectory_nominal_sub",1000);
 
-    ros::Rate rate(20.0);
+    ros::Rate loop_rate(10);
 
     while(ros::ok()){
         mavros_msgs::TrajectoryNominal msg;
 
-        msg.timestamp = 1337.0f;
+        msg.timestamp = 1337;
 
         for (int i = 0; i < 20; i++) {
-            msg.f_out[i] = i;
+            msg.f_out[i] = 3.0f;
         }
 
         traj_pub.publish(msg);
 
         ros::spinOnce();
-        rate.sleep();
+        loop_rate.sleep();
     }
 
     return 0;
