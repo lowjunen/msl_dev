f7cfb278b300e62d2ad7d891eacf064d2541b239
JunEn Low <lowjunen@gmail.com>
2020-11-01 19:23:38 -0800

    Woops. Previous commit didn't really do what I needed. So here it is. Adding custom mavros and mavlink to allow for custom traj message.

---
+++ src/mavros/mavros_extras/src/plugins/trajectory_nominal.cpp
@@ -0,0 +1,40 @@
+#include <mavros/mavros_plugin.h>
+
+#include <mavros_msgs/TrajectoryNominal.h>
+
+namespace mavros {
+namespace extra_plugins{
+    
+class TrajectoryNominalPlugin : public plugin::PluginBase {
+public:
+    TrajectoryNominalPlugin() : PluginBase(),
+        nh("~")
+    { }
+    
+    void initialize(UAS &uas_) override
+    {
+        PluginBase::initialize(uas_);
+    }
+
+    Subscriptions get_subscriptions()
+    {
+         return {/* RX disabled */ };
+    }
+    
+private:
+    ros::NodeHandle nh;
+
+    void trajectory_nominal_cb(const mavros_msgs::TrajectoryNominal::ConstPtr &req) {
+        mavlink::common::msg::TRAJECTORY_NOMINAL traj{};
+
+        traj.timestamp = req->timestamp;
+        std::copy(req->f_out.begin(), req->f_out.end(), traj.f_out.begin());
+
+        UAS_FCU(m_uas)->send_message_ignore_drop(traj);
+     }
+ };
+ }   // namespace extra_plugins
+ }   // namespace mavros
+
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(mavros::extra_plugins::TrajectoryNominalPlugin, mavros::plugin::PluginBase)
