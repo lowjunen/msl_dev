710643cc33a814fdb20051e4aa32f27547fadb2a
lowjunen <lowjunen@gmail.com>
2020-11-08 18:55:35 -0800

    Remove my first attempt at array builder.

--- src/msl_gcs/src/traj_node.cpp
+++ src/msl_gcs/src/traj_node.cpp
@@ -1,52 +1,6 @@
 #include <ros/ros.h>
 #include <mavros_msgs/TrajectoryNominal.h>
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <Eigen/Dense>
-
-using namespace std;
-using namespace Eigen;
-
-#define MAXBUFSIZE ((int) 1e6)
-
-MatrixXd readMatrix(const char *filename)
-{
-    int cols = 0, row = 0;
-    double buff[MAXBUFSIZE];
-
-    ifstream infile;
-    infile.open(filename);
-    while (! infile.eof())
-    {
-        string line;
-        getline(infile,line);
-
-        int temp_cols = 0;
-        stringstream stream(line);
-        while(! stream.eof())
-            stream >> buff[cols*rows+temp_cols++];
-            
-        if (temp_cols == 0)
-            continue;
-
-        if (cols == 0)
-            cols = temp_cols;
-        
-        rows++;
-    }
-    infile.close();
-
-    rows--;
-
-    MatrixXd result(rows,cols);
-    for (int i = 0; i < rows; i++)
-        for (int j = 0; j < cols; j++)
-            result(i,j) = buff[ cols*i+j ];
-    return result;
-};
-
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "traj_node");
