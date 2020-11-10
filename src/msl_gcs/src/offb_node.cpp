4d20c2aa20c67274744dc147f127478a589431cf
lowjunen <lowjunen@gmail.com>
2020-11-09 23:09:21 -0800

    Latest test build for basic position controller.

--- src/msl_gcs/src/offb_node.cpp
+++ src/msl_gcs/src/offb_node.cpp
@@ -39,9 +39,13 @@ int main(int argc, char **argv)
     }
 
     geometry_msgs::PoseStamped pose;
-    pose.pose.position.x = 0;
-    pose.pose.position.y = 0;
-    pose.pose.position.z = 1;
+    pose.pose.position.x = 1.25;
+    pose.pose.position.y = 1.75;
+    pose.pose.position.z = 0.00;
+    pose.pose.orientation.x = 0.00;
+    pose.pose.orientation.y = 0.00;
+    pose.pose.orientation.z = 0.00;
+    pose.pose.orientation.w = 1.00;
 
     //send a few setpoints before starting
     for(int i = 100; ros::ok() && i > 0; --i){
@@ -50,31 +54,22 @@ int main(int argc, char **argv)
         rate.sleep();
     }
 
-    mavros_msgs::SetMode offb_set_mode;
-    offb_set_mode.request.custom_mode = "OFFBOARD";
-
     mavros_msgs::CommandBool arm_cmd;
     arm_cmd.request.value = true;
 
     ros::Time last_request = ros::Time::now();
 
+    int count = 0;
+    bool check = 0;
+
     while(ros::ok()){
-        if( current_state.mode != "OFFBOARD" &&
-            (ros::Time::now() - last_request > ros::Duration(5.0))){
-            if( set_mode_client.call(offb_set_mode) &&
-                offb_set_mode.response.mode_sent){
-                ROS_INFO("Offboard enabled");
-            }
-            last_request = ros::Time::now();
-        } else {
-            if( !current_state.armed &&
-                (ros::Time::now() - last_request > ros::Duration(5.0))){
-                if( arming_client.call(arm_cmd) &&
-                    arm_cmd.response.success){
-                    ROS_INFO("Vehicle armed");
-                }
-                last_request = ros::Time::now();
-            }
+        if ( (current_state.mode == "OFFBOARD") && (count <= 100)) {
+		ROS_INFO("IN OFFBOARD");
+		count++;
+        }
+	else
+	{
+		ROS_INFO("NOT IN OFFBOARD");
         }
 
         local_pos_pub.publish(pose);
